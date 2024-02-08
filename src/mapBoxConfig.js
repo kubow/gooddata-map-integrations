b559aebb8a269196cbbd2f2f060e6cd0f6dd245f
kubow <kube.kubow@gmail.com>
2024-02-08 09:52:25 +0100

    add default mapbox config

---
+++ src/mapBoxConfig.js
@@ -0,0 +1,6 @@
+import mapboxgl from 'mapbox-gl';
+
+// Set your Mapbox access token here
+mapboxgl.accessToken = '<your-mapbox-token>'; // Replace with your Mapbox API token
+
+export default mapboxgl;

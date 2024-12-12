fe1530d4ec618d88698da6bc1712ee077a99545b
kubow <kube.kubow@gmail.com>
2024-12-12 17:34:46 +0100

    remove default code, created multiple rpojects instead

--- src/mapBoxConfig.js
+++
@@ -1,6 +0,0 @@
-import mapboxgl from 'mapbox-gl';
-
-// Set your Mapbox access token here
-mapboxgl.accessToken = '<your-mapbox-token>'; // Replace with your Mapbox API token
-
-export default mapboxgl;
